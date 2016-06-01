b439cdddfe952726f63086f1f7b5a8f2c14d84e0
佳冰 <hiqfs@qq.com>
2016-06-01 16:01:46 +0800

    2016年06月 1日  8:01:41

--- auto.sh
+++ auto.sh
@@ -2,7 +2,7 @@
 time=$(date)
 #开始处理代码
 sh web.sh
-sed -i "s/\/static/http:\/\/7xsbbu.com1.z0.glb.clouddn.com\/static/g" ./*.html #使用七牛cdn减速服务
+sed -i "s/\/static/http:\/\/hiqfs.file.alimmdn.com\/static/g" ./*.html #使用七牛cdn减速服务
 echo "$time $1" > ./static/text/version.txt
 git add --all .
 git commit -m "$time $1"
@@ -10,5 +10,5 @@ git pull
 git push github master
 git push origin master
 git push gitlab master
-sed -i "s/http:\/\/7xsbbu.com1.z0.glb.clouddn.com\/static/\/static/g" ./*.html
+sed -i "s/http:\/\/hiqfs.file.alimmdn.com\/static/\/static/g" ./*.html
 #自动部署代码
