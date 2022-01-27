e34875d0a61e1f87399bf412d1829c3f12f3758d
Zhao jiabing <hiqfs@qq.com>
2022-01-27 16:25:01 +0800

    add

--- static/js/master.js
+++ static/js/master.js
@@ -1,32 +1,23 @@
-document.ready = () => {
-    var time = setInterval(() => {
-        document.getElementById("time").innerHTML = Date();
-    }, 1000);
-    //Calculate the time
-};
-window.onload = () => {
-    //Initialization Pages
-    //Create New Play Music Event
-    MusicApiSrc = "//music.163.com/song/media/outer/url?id=";
-    PicApiSrc = "//wx1.sinaimg.cn/large/"
-    var Music = document.getElementById("music");
-    var Title = document.getElementById("title");
-    var Avatar = document.getElementById("avatar");
-    fetch('https://json.extendsclass.com/bin/bac32c6aa445')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            Music.src = MusicApiSrc + data.MusicListId[0];
-            Title.onclick = (e) => {
-                Click(MusicApiSrc, Music, data.MusicListId, true);
-            };
-            Avatar.onclick = (e) => {
-                Click(PicApiSrc, e.target, data.AvatarListId, false);
-            };
-        });
-    document.ready();
-};
-Click = (ApiUrl, Dom, List, Event) => {
+var time = setInterval(() => {
+    document.getElementById("time").innerHTML = Date();
+}, 1000);
+//Calculate the time
+var MusicApiSrc = "//music.163.com/song/media/outer/url?id=";
+var PicApiSrc = "//wx1.sinaimg.cn/large/";
+var JsonApiSrc = "https://json.extendsclass.com";
+var JsonApiId = "bac32c6aa445";
+fetch(JsonApiSrc + "/bin/bac32c6aa445")
+    .then(response => response.json())
+    .then(data => {
+        //console.log(data)
+        document.getElementById("title").onclick = (e) => {
+            Click(MusicApiSrc, document.getElementById("music"), data.MusicListId, true);
+        };
+        document.getElementById("avatar").onclick = (e) => {
+            Click(PicApiSrc, document.getElementById("avatar"), data.AvatarListId, false);
+        };
+    });
+var Click = (ApiUrl, Dom, List, Event) => {
     Dom.setAttribute('num', parseInt(Dom.getAttribute('num')) + 1);
     if (parseInt(Dom.getAttribute('num')) >= List.length) {
         Dom.setAttribute('num', 0);
@@ -36,3 +27,8 @@ Click = (ApiUrl, Dom, List, Event) => {
         Dom.play();
     }
 };
+document.addEventListener("keyup", Event => {
+    if (Event.key == "Enter") document.getElementById("title").onclick();
+    if (Event.key == "Control") document.getElementById("avatar").onclick();
+    return;
+});
