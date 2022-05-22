36aa87abd451abb9b0813582dde89394b5783663
+Bing <hiqfs@qq.com>
2022-05-22 08:38:05 +0800

    +workbox cache

---
+++ sw.js
@@ -0,0 +1,31 @@
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js',
+  'https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-core.prod.js'
+);
+let cacheSuffixVersion = 'v1'; // 缓存版本号
+const maxEntries = 100; // 最大条目数
+if (workbox) {
+    console.log(`Yay! Workbox is loaded `);
+} else {  
+    console.log(`Boo! Workbox didn't load `);
+}
+workbox.core.setCacheNameDetails({
+  prefix: '+bing', // 前缀
+  suffix: cacheSuffixVersion, // 后缀
+});
+workbox.routing.registerRoute(
+  new RegExp('^//wx1\\.sinaimg\\.cn'),
+  new workbox.strategies.CacheFirst({
+    cacheName: 'img-cache' + cacheSuffixVersion,
+    fetchOptions: {
+      mode: 'cors',
+      credentials: 'omit',
+    },
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+        purgeOnQuotaError: true,
+      }),
+    ],
+  })
+);
