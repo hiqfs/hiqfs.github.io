536af06f1c35af41b6e2020d41b79a40a0c4e525
+bing <hiqfs@qq.com>
2022-09-26 00:33:31 +0800

    hello

--- static/js/master.js
+++ static/js/master.js
@@ -1,13 +1,7 @@
-if (window.location.host == "127.0.0.1") {
-    console.log("hello this debug model");
-    Cache = "no-cache";
-} else {
-    Cache = "default";
-}
+if (window.location.host == "127.0.0.1") Cache = "no-cache";
+else Cache = "default";
 var $ = (a) => document.querySelector(a);
-var time = setInterval(() => {
-    $("#time").innerHTML = Date();
-}, 1000);
+setInterval(() => $("#time").innerHTML = Date(), 1000);
 //Calculate the time
 //init Argument
 var CDN = "https://bing-web-1251630625.cos-website.ap-nanjing.myqcloud.com/";
@@ -17,32 +11,25 @@ var Bili = "https://i0.hdslb.com/bfs/album/541402fb90c8de31a1336a18515e393bbb8a1
 var BiliImgSrc = "//i0.hdslb.com/bfs/album/";
 var JsonApiSrc = "https://json.extendsclass.com";
 var JsonApiId = "bac32c6aa445";
-fetch(JsonApiSrc + "/bin/" + JsonApiId, { cache: Cache })
+fetch(JsonApiSrc + "/bin/" + JsonApiId, { cache: Cache }) //Get Data
     .then(response => response.json())
     .then(data => {
         JsonData = data;
         data.AvatarListId.push(Bili);
         $("#music").src = MusicApiSrc + data.MusicListId[0];
         $("#avatar").src = PicApiSrc + data.AvatarListId[0];
-        $("#title").classList.add("loading");
         $("#avatar").onload = (e) => {
             //console.log(e);
             e.target.style.display = "inline";
             $("#title").classList.remove("loading");
-            document.getElementsByClassName("spinner")[0].style.display = "none";
+            $(".spinner").style.display = "none";
         }
-
-        $("#title").onclick = (e) => {
-            Click(MusicApiSrc, $("#music"), data.MusicListId, e);
-            $("#music").play();
-        };
-        $("#avatar").onclick = (e) => {
-            $("#title").classList.add("loading");
-            Click(PicApiSrc, $("#avatar"), data.AvatarListId, e);
-        };
+        $("#title").onclick = (e) => { Click(MusicApiSrc, $("#music"), data.MusicListId,e); $("#music").play(); };
+        $("#avatar").onclick = (e) => { $("#title").classList.add("loading"); Click(PicApiSrc, $("#avatar"), data.AvatarListId, e); };
     });
 var Click = (ApiUrl, Dom, List, key) => {
-    if (key) {//num--
+    if (key.offsetX>(key.srcElement.width/2)) {//num--
+        console.log("yes");
         Dom.setAttribute('num', parseInt(Dom.getAttribute('num')) + 1);
         if (parseInt(Dom.getAttribute('num')) >= List.length) {
             Dom.setAttribute('num', 0);
@@ -57,13 +44,13 @@ var Click = (ApiUrl, Dom, List, key) => {
     if (List[Dom.getAttribute('num')].substr(0, 4) == "http") {
         console.log(List[Dom.getAttribute('num')]);
         Dom.src = List[Dom.getAttribute('num')];
-        console.log("BiliBili yes");
     } else {
         //console.log(List[Dom.getAttribute('num')].substr(0, 4));
         Dom.src = ApiUrl + List[Dom.getAttribute('num')];
     }
 };
-document.addEventListener("keyup", Event => {
+/*
+document.addEventListener("keyup", Event => { //Bind Key
     switch (Event.key) {
         case "Enter":
             $("#title").onclick(true);
@@ -86,4 +73,5 @@ document.addEventListener("keyup", Event => {
     }
     return 0;
 });
+*/
 var help = () => alert("Press the Control key to switch pictures\rPress the Enter key to switch muisc");
