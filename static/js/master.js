1fe8d027b14692a65833531cb3bced27185cd584
+bing <hiqfs@qq.com>
2022-10-04 17:54:54 +0800

    bug fix

--- static/js/master.js
+++ static/js/master.js
@@ -11,6 +11,7 @@ var Bili = "https://i0.hdslb.com/bfs/album/541402fb90c8de31a1336a18515e393bbb8a1
 var BiliImgSrc = "//i0.hdslb.com/bfs/album/";
 var JsonApiSrc = "https://json.extendsclass.com";
 var JsonApiId = "bac32c6aa445";
+var PicNum = 0, MusicNum = 0;
 fetch(JsonApiSrc + "/bin/" + JsonApiId, { cache: Cache }) //Get Data
     .then(response => response.json())
     .then(data => {
@@ -24,45 +25,55 @@ fetch(JsonApiSrc + "/bin/" + JsonApiId, { cache: Cache }) //Get Data
             $("#title").classList.remove("loading");
             $(".spinner").style.display = "none";
         }
-        $("#title").onclick = (e) => { Click(MusicApiSrc, $("#music"), data.MusicListId,e); $("#music").play(); };
-        $("#avatar").onclick = (e) => { $("#title").classList.add("loading"); Click(PicApiSrc, $("#avatar"), data.AvatarListId, e); };
+        $("#title").onclick = (e) => {
+            MusicNum++;
+            MusicNum = MusicNum > 0 ? MusicNum : data.MusicListId.length + MusicNum;
+            MusicNum = MusicNum % data.MusicListId.length;
+            $("#music").src = MusicApiSrc + data.MusicListId[MusicNum];
+            $("#music").play();
+        };
+        $("#avatar").onclick = (e) => {
+            $("#title").classList.add("loading");
+            e.offsetX > (e.srcElement.width / 2) ? PicNum++ : PicNum--;
+            PicNum = PicNum > 0 ? PicNum : data.AvatarListId.length + PicNum;
+            PicNum = PicNum % data.AvatarListId.length;
+            $("#avatar").src = Click(PicApiSrc, data.AvatarListId);
+        };
     });
-var Click = (ApiUrl, Dom, List, key) => {
-    if (key.offsetX>(key.srcElement.width/2)) {//num--
-        console.log("yes");
-        Dom.setAttribute('num', parseInt(Dom.getAttribute('num')) + 1);
-        if (parseInt(Dom.getAttribute('num')) >= List.length) {
-            Dom.setAttribute('num', 0);
-        }
-    } else {//num++
-        Dom.setAttribute('num', parseInt(Dom.getAttribute('num')) - 1);
-        if (parseInt(Dom.getAttribute('num')) < 0) {
-            Dom.setAttribute('num', List.length - 1);
-        }
-    }
-
-    if (List[Dom.getAttribute('num')].substr(0, 4) == "http") {
-        console.log(List[Dom.getAttribute('num')]);
-        Dom.src = List[Dom.getAttribute('num')];
+var Click = (ApiUrl, List) => {
+    if (List[PicNum].substr(0, 4) == "http") {
+        return List[PicNum];
     } else {
-        //console.log(List[Dom.getAttribute('num')].substr(0, 4));
-        Dom.src = ApiUrl + List[Dom.getAttribute('num')];
+        return ApiUrl + List[PicNum];
     }
 };
-/*
+
 document.addEventListener("keyup", Event => { //Bind Key
     switch (Event.key) {
         case "Enter":
-            $("#title").onclick(true);
+            MusicNum++;
+            MusicNum = MusicNum > 0 ? MusicNum : data.MusicListId.length + MusicNum;
+            MusicNum = MusicNum % data.MusicListId.length;
+            $("#music").src = MusicApiSrc + data.MusicListId[MusicNum];
+            $("#music").play();
             break;
         case "Control":
-            $("#avatar").onclick(true);
+            PicNum++;
+            PicNum = PicNum > 0 ? PicNum : data.AvatarListId.length + PicNum;
+            PicNum = PicNum % data.AvatarListId.length;
+            $("#avatar").src = Click(PicApiSrc, data.AvatarListId);
             break;
         case "ArrowRight":
-            $("#avatar").onclick(true);
+            PicNum++;
+            PicNum = PicNum > 0 ? PicNum : JsonData.AvatarListId.length + PicNum;
+            PicNum = PicNum % JsonData.AvatarListId.length;
+            $("#avatar").src = Click(PicApiSrc, JsonData.AvatarListId);
             break;
         case "ArrowLeft":
-            $("#avatar").onclick(false);
+            PicNum--;
+            PicNum = PicNum > 0 ? PicNum : JsonData.AvatarListId.length + PicNum;
+            PicNum = PicNum % JsonData.AvatarListId.length;
+            $("#avatar").src = Click(PicApiSrc, JsonData.AvatarListId);
             break;
         case "ArrowUp":
             //$("#title").onclick(false);
@@ -73,5 +84,4 @@ document.addEventListener("keyup", Event => { //Bind Key
     }
     return 0;
 });
-*/
-var help = () => alert("Press the Control key to switch pictures\rPress the Enter key to switch muisc");
+//var help = () => alert("Press the Control key to switch pictures\rPress the Enter key to switch muisc");
